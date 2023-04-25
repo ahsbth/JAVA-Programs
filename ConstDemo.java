a3e6da179e11fda24598381f3441073c37aa25f2
ahsbth <asraj48342@gmail.com>
2023-04-25 23:48:06 +0530

    constructor initialization

---
+++ ConstDemo.java
@@ -0,0 +1,18 @@
+public class ConstDemo{
+    public static void main(String args[])
+    {
+       Initialize i=new Initialize();
+       i.print_val();
+    }
+}
+class Initialize{
+    int a;
+    public Initialize()
+    {
+        a=10;
+    }
+    void print_val()
+    {
+        System.out.println("Initialized value="+a);
+    }
+}
