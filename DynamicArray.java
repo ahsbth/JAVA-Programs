0d4aa4f4aaaba0eb51e9b7eeedcfdb96b383e177
ahsbth <asraj48342@gmail.com>
2023-04-26 16:03:01 +0530

    update

--- DynamicArray.java
+++ DynamicArray.java
@@ -3,10 +3,10 @@ public class DynamicArray {
     public static void main(String args[])
     {
         int sz,i;
-        int A[]=new int[sz];
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Size of Array=");
         sz=sc.nextInt();
+        int A[]=new int[sz];
         System.out.println("Enter the value for array=");
         for(i=0;i<sz;i++)
         {
