240f7f159f51c749dab61fd122b869f6dd330115
ahsbth <asraj48342@gmail.com>
2023-04-28 23:10:10 +0530

    hexadecimal literals

---
+++ Hexa.java
@@ -0,0 +1,10 @@
+public class Hexa{
+    public static void main(String args[])
+    {
+        int x=0X1AB;
+        int y=0100;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
