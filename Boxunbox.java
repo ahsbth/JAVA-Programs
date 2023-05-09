d7385abb0a33e75f5754cae824eb6ac3388da333
ahsbth <asraj48342@gmail.com>
2023-05-09 11:21:54 +0530

    boxunbox

---
+++ Boxunbox.java
@@ -0,0 +1,9 @@
+
+public class Boxunbox {
+    public static void main(String args[]){
+        Integer a=new Integer(3);    
+        int i=a.intValue(); 
+        int j=a;  
+          } 
+    }
+
